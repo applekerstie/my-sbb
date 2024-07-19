a8471cada3f1ce839204aefba4024a86bf8646dd
applekerstie <applekerstie@gmail.com>
2024-07-19 22:55:48 +0900

    list and detail

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,5 +22,14 @@ public class QuestionController {
 		model.addAttribute("questionList", questionList);
 		
 		return "question_list";
+	}
+	
+	@GetMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		
+		Question question = this.questinService.getQuestion(id);
+		model.addAttribute("question", question);
+		
+		return "question_detail";
 	}
 }
