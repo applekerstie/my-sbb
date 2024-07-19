3de1bba98cd3db0cf8ca321a4ad595e5510010cf
applekerstie <applekerstie@gmail.com>
2024-07-19 16:46:22 +0900

    repository methods

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -35,23 +35,28 @@ class SbbApplicationTests {
 		this.questionRepository.save(q2);
 		*/
 		
-		/*
 		List<Question> all = this.questionRepository.findAll();
 		assertEquals(2, all.size());
 		
 		Question q1 = all.get(0);
-		System.out.println( q1.getSubject());
-		
+		//System.out.println( q1.getSubject());
+		assertEquals("sbb가 무엇인가요?", q1.getSubject());
 		
 		Optional<Question> oq = this.questionRepository.findById(1);
 		if(oq.isPresent()) {
 			Question q2 = oq.get();
 			assertEquals("sbb가 무엇인가요?", q2.getSubject());
 		}
-		*/
 		
 		Question q3 = this.questionRepository.findBySubject("sbb가 무엇인가요?");
 		assertEquals(1, q3.getId());
+		
+		Question q4 = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다");
+		assertEquals(1, q4.getId());
+
+		List<Question> q5List = this.questionRepository.findBySubjectLike("sbb%");
+		Question q5 = q5List.get(0);
+		assertEquals("sbb가 무엇인가요?", q5.getSubject());
 		
 	}
 	
