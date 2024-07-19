0ed2bfb918b4908dd827b8c619182676ac45b518
applekerstie <applekerstie@gmail.com>
2024-07-19 17:02:46 +0900

    repository modify and delete

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,6 +16,9 @@ class SbbApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private AnswerRepository answerRepository;
 	
 	@Test
 	void testJpa() {
@@ -35,6 +39,7 @@ class SbbApplicationTests {
 		this.questionRepository.save(q2);
 		*/
 		
+		/*
 		List<Question> all = this.questionRepository.findAll();
 		assertEquals(2, all.size());
 		
@@ -42,9 +47,9 @@ class SbbApplicationTests {
 		//System.out.println( q1.getSubject());
 		assertEquals("sbb가 무엇인가요?", q1.getSubject());
 		
-		Optional<Question> oq = this.questionRepository.findById(1);
-		if(oq.isPresent()) {
-			Question q2 = oq.get();
+		Optional<Question> oq1 = this.questionRepository.findById(1);
+		if(oq1.isPresent()) {
+			Question q2 = oq1.get();
 			assertEquals("sbb가 무엇인가요?", q2.getSubject());
 		}
 		
@@ -57,6 +62,24 @@ class SbbApplicationTests {
 		List<Question> q5List = this.questionRepository.findBySubjectLike("sbb%");
 		Question q5 = q5List.get(0);
 		assertEquals("sbb가 무엇인가요?", q5.getSubject());
+		*/
+		
+	
+		Optional<Question> oq6 = this.questionRepository.findById(1);
+		assertTrue(oq6.isPresent());
+		
+		Question q6 = oq6.get();
+		q6.setSubject("수정된 제목");
+		this.questionRepository.save(q6);
+	
+		
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq7 = this.questionRepository.findById(1);
+		assertTrue(oq7.isPresent());
+		Question q7 = oq7.get();
+		this.questionRepository.delete(q7);
+		assertEquals(1, this.questionRepository.count());
+		
 		
 	}
 	
